e5e09955d9f988128eb7f9f3b8c30127dc84c25f
Giang <giangnguyen010801@gmail.com>
2022-03-02 07:08:37 +0700

    Nộp bài lần 1

---
+++ app/src/main/java/com/android/s19145369/LayoutActivity.java
@@ -0,0 +1,15 @@
+package com.android.s19145369;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.GridLayout;
+
+public class LayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
